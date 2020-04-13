7b6a085659c5b0bfd057a9febab7b865113f2824
Nickolas Reimschiissel <ryming.chizel@gmail.com>
2020-04-13 07:46:05 -0600

    ConvexHull Complete!
    *Changed drawConvexHull() to use the algorithm.
    -Removed incomplete undo functionality

--- src/GUI/ConvexHullPanel.java
+++ src/GUI/ConvexHullPanel.java
@@ -1,32 +1,21 @@
 package GUI;
 
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import javax.swing.AbstractAction;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
 
 public class ConvexHullPanel extends JPanel implements MouseListener {
 	
 	private ArrayList<Point> hullFrame = new ArrayList<Point>();
 	private ArrayList<Point> points = new ArrayList<Point>();
-	private ArrayList<Integer> actions = new ArrayList<Integer>();
-	
-	private ArrayList<ArrayList<Point>> oldInfo = new ArrayList<ArrayList<Point>>();
-	
-	final private int POINT = 1;
-	final private int DRAW = 2;
-	final private int CLEAR = 3;
 
 	public ConvexHullPanel() {
 		super();
 		
 		this.addMouseListener(this);
-		setKeys();
 	}
 	
 	public void paint(Graphics g) {
@@ -59,50 +48,23 @@ public class ConvexHullPanel extends JPanel implements MouseListener {
 		for (Point p : hullFrame) {
 			oldHullFrame.add(p);
 		}
-		oldInfo.add(oldHullFrame);
 		
 		// Save old points
 		ArrayList<Point> oldPoints = new ArrayList<Point>();
 		for (Point p : points) {
 			oldPoints.add(p);
 		}
-		oldInfo.add(oldPoints);
 		
 		// Clear old lists
 		hullFrame.clear();
 		points.clear();
-		actions.add(CLEAR);
 		repaint();
 	}
 	
 	// Method which adds the hull frame around the points (it calls the algorithm method).
 	public void drawConvexHull() {
-		for (Point p : points) {
-			hullFrame.add(p);
-		}
-		actions.add(DRAW);
+		hullFrame = ConvexHull.convexHull(points, points.size());
 		repaint();
-	}
-	
-	private void undo() {
-		// Check if an action has been performed
-		int action;
-		System.out.println(actions.size());
-		if (actions.size() == 0)
-			return;
-		else
-			action = actions.remove(actions.size()-1).intValue();
-		
-		// Undo the last action performed
-		if (action == DRAW) {
-			hullFrame.clear();
-		}
-		else if (action == POINT) {
-			points.remove(points.size()-1);
-		} else if (action == CLEAR) {
-			points = oldInfo.remove(oldInfo.size()-1);
-			hullFrame = oldInfo.remove(oldInfo.size()-2);
-		}
 	}
 	
 	//
@@ -121,26 +83,10 @@ public class ConvexHullPanel extends JPanel implements MouseListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		addPoint(e.getX(), e.getY());
-		actions.add(POINT);
 		repaint();
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) { }
-	
-	//
-	// Set key bindings
-	//
-	
-	private void setKeys() {
-		this.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "undo");
-		AbstractAction undo = new AbstractAction() {
-			public void actionPerformed(ActionEvent arg0) {
-				undo();
-				repaint();
-			}
-		};
-		this.getActionMap().put("undo", undo);
-	}
 	
 }
